3894826ed420ecc750e814b64fc3b59aa9b775f2
ProdicaI <73436076+ProdicaI@users.noreply.github.com>
2022-03-02 18:37:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ let leftWristX = 0;
 let leftWristY = 0;
 let rightWristX = 0;
 let rightWristY = 0;
+let scoreLeftWrist = 0;
+let scoreRightWrist = 0;
 
 function preload() {
   song = loadSound("./music.mp3");
@@ -43,4 +45,38 @@ function setup() {
 
 function draw() {
   image(video, 0, 0, 600, 500);
+
+  fill("#FF0000");
+  stroke("#FF0000");
+
+  if (scoreRightWrist > 0.2) {
+    circle(rightWristX, rightWristY, 20);
+
+    if (rightWristY > 0 && rightWristY <= 100) {
+      document.getElementById("speed").innerHTML = "Speed: 0.5x";
+      song.rate(0.5);
+    } else if (rightWristY > 100 && rightWristY <= 200) {
+      document.getElementById("speed").innerHTML = "Speed: 1x";
+      song.rate(1);
+    } else if (rightWristY > 200 && rightWristY <= 300) {
+      document.getElementById("speed").innerHTML = "Speed: 1.5x";
+      song.rate(1.5);
+    } else if (rightWristY > 300 && rightWristY <= 400) {
+      document.getElementById("speed").innerHTML = "Speed: 2x";
+      song.rate(2);
+    } else if (rightWristY > 400 && rightWristY <= 500) {
+      document.getElementById("speed").innerHTML = "Speed: 2.5x";
+      song.rate(2.5);
+    }
+  }
+
+  if (scoreLeftWrist > 0.2) {
+    circle(leftWristX, leftWristY, 20);
+
+    let inNumberLeftWristY = Number(leftWristY);
+    let removeDecimals = Math.floor(inNumberLeftWristY);
+    let volume = removeDecimals / 500;
+    document.getElementById("volume").innerHTML = `Volume: ${volume}`;
+    song.setVolume(volume);
+  }
 }
